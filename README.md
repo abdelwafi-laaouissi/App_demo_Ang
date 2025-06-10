3d09a0bb80789475f999c804ee2c75fa6db44f10
abdelwafi-laaouissi <laaouissi.f@gmail.com>
2025-06-10 15:59:16 +0100

    List Products V2

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ![V1-1.png](IMG/V1-1.png)
 
 ![V1-2.png](IMG/V1-2.png)
+
+
+### list products V2
+![V2.1.png](IMG/V2.1.png)
+
+![V2.2.png](IMG/V2.2.png)
+
+![V2.3.png](IMG/V2.3.png)
--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
-export class Products {
-  PRODUCTS = [
-    {id : 1 , name : "produc1", price : 2300, selected : true},
-    {id : 2 , name : "produc2", price : 5005, selected : false},
-    {id : 3 , name : "produc3", price : 330, selected : true}
-  ]
+export class Products implements OnInit{
+  PRODUCTS : any;
+  constructor() {
+  }
+  ngOnInit() {
+    this.PRODUCTS=[
+      {id : 1 , name : "produc1", price : 2300, selected : true},
+      {id : 2 , name : "produc2", price : 5005, selected : false},
+      {id : 3 , name : "produc3", price : 330, selected : true}
+    ]
+  }
+
+  handleDelete(products: any) {
+    let confi = confirm('etes vous sure de vouloir supprimer ?');
+    if (confi==true){
+      this.PRODUCTS = this.PRODUCTS.filter((p : any)=>p.id !=products.id);
+    }
+
+  }
 }
